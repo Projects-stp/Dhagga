5328f7cfd583338486116b0195cee9a4588387ce
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-23 20:19:00 +0530

    login screen added

---
+++ lib/views/auth/login_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:threads_clone/routes/routes_name.dart';
+import 'package:threads_clone/widgets/auth_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //??
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
+
+  //?? TEC ->
+  final TextEditingController emailController = TextEditingController(text: "");
+  final TextEditingController passwordController =
+      TextEditingController(text: "");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(15.0),
+            child: Form(
+              key: _form,
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/logo.png",
+                    width: 60,
+                    height: 60,
+                  ),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Text("Welcome back,"),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  AuthInput(
+                    hintText: "Enter your email",
+                    label: "Email",
+                    controller: emailController,
+                    // callback: ValidationBuilder().email().build(),
+                  ),
+                  const SizedBox(height: 30),
+                  AuthInput(
+                    hintText: "Enter your password",
+                    label: "Password",
+                    controller: passwordController,
+                    isPasswordField: true,
+                  ),
+                  const SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(
+                        const Size.fromHeight(40),
+                      ),
+                    ),
+                    child: const Text('Submit'),
+                  ),
+                  const SizedBox(height: 40),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: " Sign up",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 0.8,
+                            fontSize: 16.0,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Get.toNamed(RouteNames.register),
+                        ),
+                      ],
+                      text: "Don't have an account ?",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
